03993603e340860be79ccba6a33feeaea10ab9a7
LauritsNS20 <113506257+LauritsNS20@users.noreply.github.com>
2023-06-19 14:41:24 +0200

    Add files via upload

---
+++ gamle eksame opgave/script.js
@@ -0,0 +1,8 @@
+// en atribut er noget vi kan tilføje til et tag 
+
+// bem er en navnegivning konvation 
+
+// når vi defener et element så er det et agumenter 
+// når vi kører et element så er det et paramenter
+
+// for, while, and foreach, løkker
